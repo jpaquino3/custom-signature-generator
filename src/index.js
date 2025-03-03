d38abfeb90ee162d4016562c4d113d275da4e23a
Josh Aquino <joshaquino@MacBookPro.attlocal.net>
2025-03-02 21:19:23 -0600

    Attempting to fix the infinite scrolling issue

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reactToWebComponent from 'react-to-webcomponent';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
-// Wrap the App component as a web component
-const WebComponent = reactToWebComponent(App, React, ReactDOM);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// Register the custom element (note: custom element names must contain a hyphen)
-customElements.define('signature-generator', WebComponent);
-
-// Optionally, remove the traditional mounting if you only want to use the web component
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
